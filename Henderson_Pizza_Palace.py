82c0daf94ce1496a8ebc336748b303ca0e009a49
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2022-03-24 10:13:31 +1300

    Update Henderson_Pizza_Palace.py

--- Henderson_Pizza_Palace.py
+++ Henderson_Pizza_Palace.py
@@ -4,7 +4,24 @@ from tkinter import *
 #Dictionary to contain all of the information from the user
 order = {'delivery': False, 'name': '', 'address': '', 'phone': ''}
 
+#set up the variables
 window = Tk()
+top = Frame(window)
+buttons = Frame(window)
 
+#set up for the window
+top.pack()
+buttons.pack()
+
+label1 = Label(top, text = "Do you want your order delivered or will you pick it up?")
+label1.pack()
+
+deliverbutton = Button(buttons, text = 'Delivery')
+deliverbutton.pack()
+
+pickupbutton = Button(buttons, text = 'Pick up')
+pickupbutton.pack()
+
+#execute the set up
 window.mainloop()
 
