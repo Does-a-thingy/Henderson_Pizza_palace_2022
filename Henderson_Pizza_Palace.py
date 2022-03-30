54a5a64d5f6f5b21a70f38700f2fb2df0ce5d50a
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2022-03-31 10:58:24 +1300

    Update Henderson_Pizza_Palace.py

--- Henderson_Pizza_Palace.py
+++ Henderson_Pizza_Palace.py
@@ -1,11 +1,13 @@
 #Importing tkinter to use for creating a GUI
 from tkinter import *
+from tkinter.ttk import *
 
 #Dictionary to contain all of the information from the user
 order = {'delivery': False, 'name': '', 'address': '', 'phone': ''}
 
 #set up the tk variables
 window = Tk()
+window.title('Henderson Pizza Place')
 top = Frame(window)
 deliveryframe = Frame(window)
 nameframe = Frame(window)
@@ -18,7 +20,10 @@ button2texts = ['Pick up', '']
 #set up text variables
 labelling1 = StringVar()
 labelling1.set(labeltexts[0])
-
+buttoning1 = StringVar()
+buttoning1.set(button1texts[0])
+buttoning2 = StringVar()
+buttoning2.set(button2texts[0])
 #set up counting variables
 x = 1
 part = 0
@@ -31,6 +36,8 @@ def update_window():
         x += 1
         part += 1
         labelling1.set(labeltexts[part])
+        hide_widget(deliverbutton)
+        hide_widget(pickupbutton)
     elif x == 2:
         print('second')
 
@@ -39,21 +46,23 @@ def update_deliver():
     update_window()
 
 def address_update():
-    
+    print('b')
 
+def hide_widget(widget):
+    widget.pack_forget()
 #set up for the window
 top.grid(row=0, column=0, padx=10, pady=10)
-buttons.grid(row=1, column=0, padx=10, pady=10, sticky='NSEW')
+deliveryframe.grid(row=1, column=0, padx=10, pady=10, sticky='NSEW')
 nameframe.grid(row=2, column=0, padx=10, pady=10, sticky='NSEW')
 
 label1 = Label(top, textvariable = labelling1)
 label1.grid(row=0, column=0, padx=10, pady=3)
 
 #delivery or pick up buttons
-deliverbutton = Button(deliveryframe, textvariable=button1texts, command=update_deliver)
+deliverbutton = Button(deliveryframe, textvariable=buttoning1, command=update_deliver)
 deliverbutton.grid(row=0, column=0, padx=10, pady=3, columnspan=2, sticky='WE')
 
-pickupbutton = Button(deliveryframe, textvariable=button2texts, command=update_window)
+pickupbutton = Button(deliveryframe, textvariable=buttoning2, command=update_window)
 pickupbutton.grid(row=1, column=0, padx=10, pady=3, columnspan=2, sticky='WE')
 
 #getting name entry and submit button
@@ -61,8 +70,11 @@ name = DoubleVar()
 name.set('')
 
 name_label = Label(nameframe, text='Name:')
-name_lavel.grid(row=0)
+name_label.grid(row=0, column=0, padx=10, pady=3)
 
+name_entry = Entry(nameframe, textvariable=name)
+name_entry.grid(row=0, column=1, padx=10, pady=3, sticky='WE')
+
+nameframe.pack_forget()
 #execute the set up
 window.mainloop()
-
