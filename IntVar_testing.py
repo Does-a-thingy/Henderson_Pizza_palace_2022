b9c8c89fa29805121ef2de7d45d548ca9db541ab
Does-a-thingy <79290020+Does-a-thingy@users.noreply.github.com>
2023-06-22 10:51:26 +1200

    testing for lv3 exam

--- IntVar_testing.py
+++ IntVar_testing.py
@@ -14,13 +14,15 @@ Label(win, textvariable = showthing).pack()
 
 def show():
     global total_list, total_number
-    total_number.set(total_number.get() + 2)
+    total_number.set(total_number.get() + num2.get())
     showthing.set(total_number.get())
 
 num1.set(4)
 num2.set(7)
 
-total_number.set(num1.get() + 2)
+total_number.set(num1.get() + num2.get())
+
+showthing.set(total_number.get())
 
 Button(win, text='show', command=show).pack()
 
